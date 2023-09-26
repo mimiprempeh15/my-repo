55e97d1ab7d034852b029a64ae3401859061ebf8
Miriam Bosompim <nana.mb1984@gmail.com>
2023-09-26 21:53:23 +0000

    my first commit

---
+++ Maven/app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+echo "Please enter your name"
+read name
+echo "$name, welcome to Bank of America Bank"
+echo "Please enter your pin"
+read -s pin  # -s pass a secret variable
+if (( $pin == 1234 ))
+then 
+echo "You have entered the correct pin"
+fi
